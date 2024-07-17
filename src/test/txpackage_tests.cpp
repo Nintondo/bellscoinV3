478b218c51599fca051f32d366200268782add5d
Dmitry Matsiukhov <46869283+dimmarvel@users.noreply.github.com>
2024-07-17 15:07:24 +0300

    Fix windows guix build (#4)

--- src/test/txpackage_tests.cpp
+++ src/test/txpackage_tests.cpp
@@ -136,100 +136,100 @@ BOOST_FIXTURE_TEST_CASE(package_validation_tests, TestChain100Setup)
     // BOOST_CHECK_EQUAL(m_node.mempool->size(), initialPoolSize);
 }
 
-BOOST_FIXTURE_TEST_CASE(noncontextual_package_tests, TestChain100Setup)
-{
-    // The signatures won't be verified so we can just use a placeholder
-    CKey placeholder_key;
-    placeholder_key.MakeNewKey(true);
-    CScript spk = GetScriptForDestination(PKHash(placeholder_key.GetPubKey()));
-    CKey placeholder_key_2;
-    placeholder_key_2.MakeNewKey(true);
-    CScript spk2 = GetScriptForDestination(PKHash(placeholder_key_2.GetPubKey()));
+// BOOST_FIXTURE_TEST_CASE(noncontextual_package_tests, TestChain100Setup)
+// {
+//     // The signatures won't be verified so we can just use a placeholder
+//     CKey placeholder_key;
+//     placeholder_key.MakeNewKey(true);
+//     CScript spk = GetScriptForDestination(PKHash(placeholder_key.GetPubKey()));
+//     CKey placeholder_key_2;
+//     placeholder_key_2.MakeNewKey(true);
+//     CScript spk2 = GetScriptForDestination(PKHash(placeholder_key_2.GetPubKey()));
 
-    // Parent and Child Package
-    // {
-    //     auto mtx_parent = CreateValidMempoolTransaction(m_coinbase_txns[0], 0, 0, coinbaseKey, spk,
-    //                                                     CAmount(49 * COIN), /*submit=*/false);
-    //     CTransactionRef tx_parent = MakeTransactionRef(mtx_parent);
+//     Parent and Child Package
+//     {
+//         auto mtx_parent = CreateValidMempoolTransaction(m_coinbase_txns[0], 0, 0, coinbaseKey, spk,
+//                                                         CAmount(49 * COIN), /*submit=*/false);
+//         CTransactionRef tx_parent = MakeTransactionRef(mtx_parent);
 
-    //     auto mtx_child = CreateValidMempoolTransaction(tx_parent, 0, 101, placeholder_key, spk2,
-    //                                                    CAmount(48 * COIN), /*submit=*/false);
-    //     CTransactionRef tx_child = MakeTransactionRef(mtx_child);
+//         auto mtx_child = CreateValidMempoolTransaction(tx_parent, 0, 101, placeholder_key, spk2,
+//                                                        CAmount(48 * COIN), /*submit=*/false);
+//         CTransactionRef tx_child = MakeTransactionRef(mtx_child);
 
-    //     PackageValidationState state;
-    //     BOOST_CHECK(CheckPackage({tx_parent, tx_child}, state));
-    //     BOOST_CHECK(!CheckPackage({tx_child, tx_parent}, state));
-    //     BOOST_CHECK_EQUAL(state.GetResult(), PackageValidationResult::PCKG_POLICY);
-    //     BOOST_CHECK_EQUAL(state.GetRejectReason(), "package-not-sorted");
-    //     BOOST_CHECK(IsChildWithParents({tx_parent, tx_child}));
-    //     BOOST_CHECK(IsChildWithParentsTree({tx_parent, tx_child}));
-    // }
+//         PackageValidationState state;
+//         BOOST_CHECK(CheckPackage({tx_parent, tx_child}, state));
+//         BOOST_CHECK(!CheckPackage({tx_child, tx_parent}, state));
+//         BOOST_CHECK_EQUAL(state.GetResult(), PackageValidationResult::PCKG_POLICY);
+//         BOOST_CHECK_EQUAL(state.GetRejectReason(), "package-not-sorted");
+//         BOOST_CHECK(IsChildWithParents({tx_parent, tx_child}));
+//         BOOST_CHECK(IsChildWithParentsTree({tx_parent, tx_child}));
+//     }
 
-    // 24 Parents and 1 Child
-    // {
-    //     Package package;
-    //     CMutableTransaction child;
-    //     for (int i{0}; i < 24; ++i) {
-    //         auto parent = MakeTransactionRef(CreateValidMempoolTransaction(m_coinbase_txns[i + 1],
-    //                                          0, 0, coinbaseKey, spk, CAmount(48 * COIN), false));
-    //         package.emplace_back(parent);
-    //         child.vin.emplace_back(COutPoint(parent->GetHash(), 0));
-    //     }
-    //     child.vout.emplace_back(47 * COIN, spk2);
+//     24 Parents and 1 Child
+//     {
+//         Package package;
+//         CMutableTransaction child;
+//         for (int i{0}; i < 24; ++i) {
+//             auto parent = MakeTransactionRef(CreateValidMempoolTransaction(m_coinbase_txns[i + 1],
+//                                              0, 0, coinbaseKey, spk, CAmount(48 * COIN), false));
+//             package.emplace_back(parent);
+//             child.vin.emplace_back(COutPoint(parent->GetHash(), 0));
+//         }
+//         child.vout.emplace_back(47 * COIN, spk2);
 
-    //     // The child must be in the package.
-    //     BOOST_CHECK(!IsChildWithParents(package));
+//         // The child must be in the package.
+//         BOOST_CHECK(!IsChildWithParents(package));
 
-    //     // The parents can be in any order.
-    //     FastRandomContext rng;
-    //     Shuffle(package.begin(), package.end(), rng);
-    //     package.push_back(MakeTransactionRef(child));
+//         // The parents can be in any order.
+//         FastRandomContext rng;
+//         Shuffle(package.begin(), package.end(), rng);
+//         package.push_back(MakeTransactionRef(child));
 
-    //     PackageValidationState state;
-    //     BOOST_CHECK(CheckPackage(package, state));
-    //     BOOST_CHECK(IsChildWithParents(package));
-    //     BOOST_CHECK(IsChildWithParentsTree(package));
+//         PackageValidationState state;
+//         BOOST_CHECK(CheckPackage(package, state));
+//         BOOST_CHECK(IsChildWithParents(package));
+//         BOOST_CHECK(IsChildWithParentsTree(package));
 
-    //     package.erase(package.begin());
-    //     BOOST_CHECK(IsChildWithParents(package));
+//         package.erase(package.begin());
+//         BOOST_CHECK(IsChildWithParents(package));
 
-    //     // The package cannot have unrelated transactions.
-    //     package.insert(package.begin(), m_coinbase_txns[0]);
-    //     BOOST_CHECK(!IsChildWithParents(package));
-    // }
+//         // The package cannot have unrelated transactions.
+//         package.insert(package.begin(), m_coinbase_txns[0]);
+//         BOOST_CHECK(!IsChildWithParents(package));
+//     }
 
-    // // 2 Parents and 1 Child where one parent depends on the other.
-    // {
-    //     CMutableTransaction mtx_parent;
-    //     mtx_parent.vin.emplace_back(COutPoint(m_coinbase_txns[0]->GetHash(), 0));
-    //     mtx_parent.vout.emplace_back(20 * COIN, spk);
-    //     mtx_parent.vout.emplace_back(20 * COIN, spk2);
-    //     CTransactionRef tx_parent = MakeTransactionRef(mtx_parent);
+//     // 2 Parents and 1 Child where one parent depends on the other.
+//     {
+//         CMutableTransaction mtx_parent;
+//         mtx_parent.vin.emplace_back(COutPoint(m_coinbase_txns[0]->GetHash(), 0));
+//         mtx_parent.vout.emplace_back(20 * COIN, spk);
+//         mtx_parent.vout.emplace_back(20 * COIN, spk2);
+//         CTransactionRef tx_parent = MakeTransactionRef(mtx_parent);
 
-    //     CMutableTransaction mtx_parent_also_child;
-    //     mtx_parent_also_child.vin.emplace_back(COutPoint(tx_parent->GetHash(), 0));
-    //     mtx_parent_also_child.vout.emplace_back(20 * COIN, spk);
-    //     CTransactionRef tx_parent_also_child = MakeTransactionRef(mtx_parent_also_child);
+//         CMutableTransaction mtx_parent_also_child;
+//         mtx_parent_also_child.vin.emplace_back(COutPoint(tx_parent->GetHash(), 0));
+//         mtx_parent_also_child.vout.emplace_back(20 * COIN, spk);
+//         CTransactionRef tx_parent_also_child = MakeTransactionRef(mtx_parent_also_child);
 
-    //     CMutableTransaction mtx_child;
-    //     mtx_child.vin.emplace_back(COutPoint(tx_parent->GetHash(), 1));
-    //     mtx_child.vin.emplace_back(COutPoint(tx_parent_also_child->GetHash(), 0));
-    //     mtx_child.vout.emplace_back(39 * COIN, spk);
-    //     CTransactionRef tx_child = MakeTransactionRef(mtx_child);
+//         CMutableTransaction mtx_child;
+//         mtx_child.vin.emplace_back(COutPoint(tx_parent->GetHash(), 1));
+//         mtx_child.vin.emplace_back(COutPoint(tx_parent_also_child->GetHash(), 0));
+//         mtx_child.vout.emplace_back(39 * COIN, spk);
+//         CTransactionRef tx_child = MakeTransactionRef(mtx_child);
 
-    //     PackageValidationState state;
-    //     BOOST_CHECK(IsChildWithParents({tx_parent, tx_parent_also_child}));
-    //     BOOST_CHECK(IsChildWithParents({tx_parent, tx_child}));
-    //     BOOST_CHECK(IsChildWithParents({tx_parent, tx_parent_also_child, tx_child}));
-    //     BOOST_CHECK(!IsChildWithParentsTree({tx_parent, tx_parent_also_child, tx_child}));
-    //     // IsChildWithParents does not detect unsorted parents.
-    //     BOOST_CHECK(IsChildWithParents({tx_parent_also_child, tx_parent, tx_child}));
-    //     BOOST_CHECK(CheckPackage({tx_parent, tx_parent_also_child, tx_child}, state));
-    //     BOOST_CHECK(!CheckPackage({tx_parent_also_child, tx_parent, tx_child}, state));
-    //     BOOST_CHECK_EQUAL(state.GetResult(), PackageValidationResult::PCKG_POLICY);
-    //     BOOST_CHECK_EQUAL(state.GetRejectReason(), "package-not-sorted");
-    // }
-}
+//         PackageValidationState state;
+//         BOOST_CHECK(IsChildWithParents({tx_parent, tx_parent_also_child}));
+//         BOOST_CHECK(IsChildWithParents({tx_parent, tx_child}));
+//         BOOST_CHECK(IsChildWithParents({tx_parent, tx_parent_also_child, tx_child}));
+//         BOOST_CHECK(!IsChildWithParentsTree({tx_parent, tx_parent_also_child, tx_child}));
+//         // IsChildWithParents does not detect unsorted parents.
+//         BOOST_CHECK(IsChildWithParents({tx_parent_also_child, tx_parent, tx_child}));
+//         BOOST_CHECK(CheckPackage({tx_parent, tx_parent_also_child, tx_child}, state));
+//         BOOST_CHECK(!CheckPackage({tx_parent_also_child, tx_parent, tx_child}, state));
+//         BOOST_CHECK_EQUAL(state.GetResult(), PackageValidationResult::PCKG_POLICY);
+//         BOOST_CHECK_EQUAL(state.GetRejectReason(), "package-not-sorted");
+//     }
+// }
 
 BOOST_FIXTURE_TEST_CASE(package_submission_tests, TestChain100Setup)
 {
